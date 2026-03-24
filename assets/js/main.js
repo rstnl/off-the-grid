fdaa27a827e69a561eb2f9830a919ab02f8e930e
rstnl <chicagosnowman23@gmail.com>
2026-03-24 17:33:28 +0000

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 // main.js
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+}
 
-console.log("Hello, World!");
+let divs = document.querySelectorAll(".grid-container div");
+function randomize() {
+    divs.forEach(function(div){
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0,20);
+        let rotate = randomNumber(0, 360);
+        div.computedStyleMap.transform = 
+        'scale(${scale})
+        translate(${translateX}%, ${translateY}%)
+        rotate(${rotate}deg)'
+    })
+
+    
+}
