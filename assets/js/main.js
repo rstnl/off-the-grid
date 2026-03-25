32284e039536942b39c10f41fe6d59dff2fc876f
rstnl <chicagosnowman23@gmail.com>
2026-03-25 17:19:15 +0000

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
 // main.js
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (max - min) + 1) + min;
-}
 
+let grid = document.querySelectorAll(".grid-container");
 let divs = document.querySelectorAll(".grid-container div");
+
 function randomize() {
-    divs.forEach(function(div){
+    console.log(divs.length, "randomize!")
+
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+    divs.forEach(function (div) {
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
-        let translateY = randomNumber(0,20);
+        let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
-        div.computedStyleMap.transform = 
-        'scale(${scale})
-        translate(${translateX}%, ${translateY}%)
-        rotate(${rotate}deg)'
-    })
 
-    
+        // 50% of the time ...
+        if (Math.random() > .5) {
+            scale = 1; // don't affect scale
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
+
+        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
+}
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
